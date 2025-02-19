4014733596b733293d9ed1dcce82a9e67a571312
otoolz <91725382+otoolz@users.noreply.github.com>
2025-02-19 11:27:08 +0900

    [Refactor] 디스크캐시에 etag 추가 가능하도록 수정

--- Data/Cache/DiskCache.swift
+++ Data/Cache/DiskCache.swift
@@ -11,6 +11,7 @@ class DiskCache {
     
     private let fileManager: FileManager = .init()
     private let cachedDataName: String = "cachedData"
+    private let etagFileName: String = "etag"
     
     func fetch(forKey key: String) -> Data? {
         guard let cacheDirectoryURL = makeCacheDirectoryURL(forKey: key) else { return nil }
@@ -18,11 +19,27 @@ class DiskCache {
         return try? Data(contentsOf: cacheDataURL)
     }
     
-    func store(_ data: Data, forKey key: String) {
+    func store(_ data: Data, etag: String? = nil, forKey key: String) {
         guard let cacheDirectoryURL = makeCacheDirectoryURL(forKey: key) else { return }
         let cacheDataURL = cacheDirectoryURL.appending(path: cachedDataName)
         try? fileManager.createDirectory(atPath: cacheDirectoryURL.path(), withIntermediateDirectories: true)
         fileManager.createFile(atPath: cacheDataURL.path(), contents: data)
+        
+        if let etag {
+            let etagURL = cacheDirectoryURL.appendingPathComponent(etagFileName)
+            try? etag.write(to: etagURL, atomically: true, encoding: .utf8)
+        }
+    }
+    
+    func etag(forKey key: String) -> String? {
+        guard let cacheDirectoryURL = makeCacheDirectoryURL(forKey: key) else { return nil }
+        let etagURL = cacheDirectoryURL.appendingPathComponent(etagFileName)
+        return try? String(contentsOfFile: etagURL.path, encoding: .utf8)
+    }
+    
+    func remove(forKey key: String) {
+        guard let cacheDirectoryURL = makeCacheDirectoryURL(forKey: key) else { return }
+        try? fileManager.removeItem(at: cacheDirectoryURL)
     }
     
     private func makeCacheDirectoryURL(forKey key: String) -> URL? {
