1e97df44241e053d3efbd93f347d2c7dfce97530
otoolz <91725382+otoolz@users.noreply.github.com>
2025-02-19 11:26:58 +0900

    [Refactor] 메모리캐시에 etag 추가 가능하도록 수정

--- Data/Cache/MemoryCache.swift
+++ Data/Cache/MemoryCache.swift
@@ -9,13 +9,32 @@ import Foundation
 
 class MemoryCache {
     
-    private let memoryCache: NSCache<NSString, NSData> = .init()
-    
-    func fetch(forKey key: String) -> Data? {
-        return memoryCache.object(forKey: key as NSString) as? Data
+    private class CacheItem {
+        let data: Data
+        let etag: String?
+        
+        init(data: Data, etag: String?) {
+            self.data = data
+            self.etag = etag
+        }
     }
     
-    func store(_ data: Data, forKey key: String) {
-        memoryCache.setObject(data as NSData, forKey: key as NSString)
+    private let cache: NSCache<NSString, CacheItem> = .init()
+    
+    func fetch(forKey key: String) -> Data? {
+        return cache.object(forKey: key as NSString)?.data
+    }
+    
+    func store(_ data: Data, etag: String? = nil, forKey key: String) {
+        let cacheItem = CacheItem(data: data, etag: etag)
+        cache.setObject(cacheItem, forKey: key as NSString)
+    }
+    
+    func etag(forKey key: String) -> String? {
+        return cache.object(forKey: key as NSString)?.etag
+    }
+    
+    func remove(forKey key: String) {
+        cache.removeObject(forKey: key as NSString)
     }
 }
