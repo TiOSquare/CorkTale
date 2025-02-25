151205618a1658c69f451fd30210b694db07c0cc
otoolz <91725382+otoolz@users.noreply.github.com>
2025-02-25 17:56:09 +0900

    [FIX] Wine Model property public 키워드 추가

--- Domain/Entities/Wine.swift
+++ Domain/Entities/Wine.swift
@@ -5,14 +5,14 @@
 //  Created by hyerim.jin on 2/13/25.
 //
 
-public struct Wine {
-    let id: Int
-    let name: String
-    let link: String
-    let thumb: String?
-    let averageRating: Double?
-    let ratings: Int?
-    let price: Double?
+public struct Wine: Equatable, Identifiable {
+    public let id: Int
+    public let name: String
+    public let link: String
+    public let thumb: String
+    public let averageRating: Double?
+    public let ratings: Int?
+    public let price: Double?
     
     public init(
         id: Int,
@@ -26,7 +26,7 @@ public struct Wine {
         self.id = id
         self.name = name
         self.link = link
-        self.thumb = thumb
+        self.thumb = thumb ?? ""
         self.averageRating = averageRating
         self.ratings = ratings
         self.price = price
