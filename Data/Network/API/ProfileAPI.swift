b9a9765dccffc3e6104319fee2a3e5c5ab108508
gayoung.kim <gayoung.kim@tisquare.com>
2025-02-27 18:32:31 +0900

    [REFACTOR] 	createProfile(프로필생성) API 요청 방식 리팩토링
     - 기존 함수시그니처를 ProfileDTO를 사용하는 방식으로 변경
     - JSON 인코딩 변환로직 제거 후 requestJSONEncodable 사용방식으로 변경

--- Data/Network/API/ProfileAPI.swift
+++ Data/Network/API/ProfileAPI.swift
@@ -10,7 +10,7 @@ import Moya
 import Shared
 
 enum ProfileAPI {
-    case createProfile(String, String, Int, String, [String])
+    case createProfile(ProfileDTO)
     case getAllProfile
 }
 
@@ -44,18 +44,8 @@ extension ProfileAPI: TargetType {
     
     var task: Task {
         switch self {
-        case .createProfile(let nickname, let profileImage, let level, let nationality, let emblem):
-            let dto = ProfileDTO(nickname: nickname,
-                                 profileImage: profileImage,
-                                 level: level,
-                                 nationality: nationality,
-                                 emblem: emblem)
-            if let jsonData = try? JSONEncoder().encode(dto),
-               let jsonObject = try? JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: Any] {
-                return .requestParameters(parameters: jsonObject, encoding: JSONEncoding.default)
-            } else {
-                return .requestPlain
-            }
+        case .createProfile(let dto):
+            return .requestJSONEncodable(dto)
         case .getAllProfile:
             return .requestPlain
         }
--- Data/Repositories/ProfileRepositoryImpl.swift
+++ Data/Repositories/ProfileRepositoryImpl.swift
@@ -15,8 +15,13 @@ public final class ProfileRepositoryImpl: ProfileRepository {
     public init() { }
     
     public func createProfile(nickName: String, profileImage: String, level: Int, nationality: String, emblem: [String]) async throws -> Profile {
+        let profileDTO = ProfileDTO(nickname: nickName,
+                                    profileImage: profileImage,
+                                    level: level,
+                                    nationality: nationality,
+                                    emblem: emblem)
         let response: RestResponse<ProfileDTO> = try await
-        self.provider.request(ProfileAPI.createProfile(nickName, profileImage, level, nationality, emblem))
+        self.provider.request(ProfileAPI.createProfile(profileDTO))
         
         return response.data!.toDomain()
     }
