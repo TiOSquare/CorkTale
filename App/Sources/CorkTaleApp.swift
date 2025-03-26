05d8b7771bcf7d20be1bcb8edb6c3a41aa1fbdfc
otoolz <91725382+otoolz@users.noreply.github.com>
2025-03-26 15:52:32 +0900

    [FIX] 앱 이니셜 뷰 수정
    메인 탭 뷰를 초기 화면으로 수정

--- App/Sources/CorkTaleApp.swift
+++ App/Sources/CorkTaleApp.swift
@@ -7,11 +7,7 @@ import Domain
 struct CorkTaleApp: App {
     var body: some Scene {
         WindowGroup {
-//            ContentView()
-//            CameraView(store: .init(initialState: CameraFeature.State(), reducer: { CameraFeature() } ))
-//            WineSearchView(store: .init(initialState: WineSearchFeature.State(), reducer: { WineSearchFeature() } ))
-            WineStoreMapView(store: .init(initialState: WineStoreMapFeature.State(),
-                                          reducer: { WineStoreMapFeature(useCase: LocationUseCaseImpl()) } ))
+            ContentView()
         }
     }
 }
