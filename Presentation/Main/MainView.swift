7ef65e7d2d5890425b004dd66f89ae9d9ce1f231
otoolz <91725382+otoolz@users.noreply.github.com>
2025-03-31 16:55:10 +0900

    [FEAT] TabBar 테스트 코드 추가

--- Presentation/Main/MainView.swift
+++ Presentation/Main/MainView.swift
@@ -14,12 +14,83 @@ public struct MainView: View {
     private let factory: StoreFactory
     private let store: StoreOf<MainFeature>
     
+    @State private var selectedTab: TabItem = .home
+    
     public init(factory: StoreFactory) {
         self.factory = factory
         self.store = factory.makeMainFeatureStore()
     }
     
     public var body: some View {
+//        body1()
+//        body2()
+//        body3()
+        body4()
+    }
+    
+    private func body1() -> some View {
+        VStack(spacing: 0) {
+            TabView(selection: $selectedTab) {
+                ForEach(TabItem.allCases, id: \.self) { item in
+                    
+                    CurrentMainView(for: item)
+                        .tabItem {
+                            Image(systemName: item.image)
+                            Text(item.text)
+                        }
+                        .tag(item)
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .animation(.easeInOut, value: selectedTab)
+            
+        }
+    }
+    
+    private func body2() -> some View {
+        VStack(spacing: 0) {
+            TabView {
+                ForEach(TabItem.allCases, id: \.self) { item in
+                    
+                    CurrentMainView(for: item)
+                        .tag(item)
+                        .tabItem {
+                            Image(systemName: item.image)
+                            Text(item.text)
+                        }
+                        .badge(3)
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .animation(.easeInOut, value: store.selectedTab)
+            
+        }
+    }
+    
+    @ViewBuilder
+    private func body3() -> some View {
+        if #available(iOS 18.0, *) {
+            VStack(spacing: 0) {
+                TabView(selection: $selectedTab) {
+                    ForEach(TabItem.allCases, id: \.self) { item in
+                        
+                        Tab(item.text, systemImage: item.image, value: item) {
+                            CurrentMainView(for: item)
+                        }
+                        
+//                        Tab(item.text, systemImage: item.image) {
+//                            CurrentMainView(for: item)
+//                        }
+                        .badge("!")
+                    }
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .animation(.easeInOut, value: store.selectedTab)
+            }
+        }
+    }
+    
+    private func body4() -> some View {
         GeometryReader { geometry in
             VStack(spacing: 0) {
                 Group {
